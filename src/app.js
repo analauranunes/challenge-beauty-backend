185af0601d67a91dff79bcc58937fd1b0c3284bb
Ana Laura <analaurahdn@gmail.com>
2022-06-18 15:21:28 -0300

    fix: nodemon.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
+import express from "express";
 import loginRouter from "./routes/login";
 import userRouter from "./routes/users";
-
-const express = require("express");
 
 const app = express();
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-app.use("/users", userRouter);
-app.use("/login", loginRouter);
-console.log(port);
+app.use("/users", userRouter)
+app.use("/login", loginRouter)
+console.log(port)
 
 app.listen(port, () => console.log("Aplication running"));
--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -1,10 +1,9 @@
+import { Router } from "express";
 import { expressYupMiddleware } from "express-yup-middleware";
 import loginController from "../../controllers/login/login.controller";
 import loginSchema from "../../schemas/login/login.schema"
 
-const {Router} = require('express');
-
-const router = Router()
+const router = Router();
 
 router.post("", expressYupMiddleware({schemaValidator: loginSchema}), loginController)
 
