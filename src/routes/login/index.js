701612e5d08afb908be50699eff0ca8d8d374f69
Ana Laura <analaurahdn@gmail.com>
2022-06-18 15:17:11 -0300

    fix: router importation fixed.

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -1,9 +1,10 @@
-import { Router } from "express";
 import { expressYupMiddleware } from "express-yup-middleware";
 import loginController from "../../controllers/login/login.controller";
 import loginSchema from "../../schemas/login/login.schema"
 
-const router = Router();
+const {Router} = require('express');
+
+const router = Router()
 
 router.post("", expressYupMiddleware({schemaValidator: loginSchema}), loginController)
 
--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -1,8 +1,9 @@
-import { Router } from "express";
 import { expressYupMiddleware } from "express-yup-middleware";
 import allUsersController from "../../controllers/users/allUsers.controller";
 import createUserController from "../../controllers/users/creteUser.controller";
 import createUserSchema from "../../schemas/users/createUser.schema";
+
+const { Router } = require("express");
 
 const router = Router();
 
