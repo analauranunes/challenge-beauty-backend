d6518daddddcbc1e86ea8e9fb3070c73b10bad98
Ana Laura <analaurahdn@gmail.com>
2022-06-23 16:39:50 -0300

    fix: url from heroku added to database.

--- src/database/users/index.js
+++ src/database/users/index.js
@@ -2,11 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "anahsn",
-  host: "localhost",
-  database: "challenge_beauty_pass",
-  password: "Cake!chocolate@1",
-  port: 5432,
+  url: "https://my-beauty-pass.herokuapp.com/",
 });
 
 module.exports = { pool };
-
